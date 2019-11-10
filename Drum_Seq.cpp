6d23b02e2b58b7794add360dfd74d9dd28069533
Shaneth <shanethp@gmail.com>
2019-11-10 04:17:27 -0500

    Made significant progress on the matrix class

--- Drum_Seq.cpp
+++ Drum_Seq.cpp
@@ -11,9 +11,13 @@
 //Libraries
 #include <Arduino.h>
 #include <Adafruit_NeoTrellis.h>
+#include <algorithm>
+#include <vector>
 
 //--------------------------------------------------------------------------------------------------------------------------
 //Enviornment Constants
+#define MAXLENGTH 64 //only god knows what will actually happen if this variable changes. Leaving it at 64 for now.
+#define STARTINGVELOCITY 75
 
 //Menu System
 #define menuLBtn 0
@@ -30,22 +34,84 @@ const int chLeds[4] = {33, 34, 35, 36};
 //Matrix
 #define matrixHeight 4
 #define matrixWidth 8
+#define selBlinkTime 200
+#define selTrackBlueOffset 127
+#define selTrackOffColor 0x0000AA
+
+//--------------------------------------------------------------------------------------------------------------------------
+//Global Variables
+int step = 0;
 
 //--------------------------------------------------------------------------------------------------------------------------
 //Classes
 class Track
 {
-
-}
+public:
+    //these are all int8_t to minimize memory usage as I will be creating a lot of these.
+    static int8_t numTracks;
+    int8_t id, pitch, channel;
+    int8_t notes[MAXLENGTH]; //note values are normally 0-127. however, instead of creating a note class so that I could also store a selected boolean, I'm just going to flip the sign
+    Track()
+    {
+        id = numTracks;
+        numTracks++;          //static values ftw for ID'ing all these tracks and setting pitches automatically
+        pitch = 36 + 16 - id; //to follow GM mapping chromatically
+        channel = 1;
+    }
+    int getNote(int n)
+    {
+        return abs(notes[n]);
+    }
+    int getNote()
+    {
+        return abs(notes[step]);
+    }
+    bool noteSelected(int n)
+    {
+        return (notes[n] < 0);
+    }
+    bool noteSelected()
+    {
+        return (notes[step] < 0);
+    }
+    void selectNote(int n)
+    {
+        if (notes[n] > 0)
+        {
+            notes[n] = -notes[n];
+        }
+    }
+    void toggleNoteSel(int n)
+    {
+        notes[n] = -notes[n];
+    }
+    void toggleNoteOn(int n)
+    {
+        if (notes[n] == 0)
+        {
+            notes[n] = STARTINGVELOCITY;
+        }
+    }
+    void deselectAllNotes()
+    {
+        for (int i = 0; i < MAXLENGTH; i++)
+        {
+            notes[i] = abs(notes[i]); //using sign flip to indicate selected was a good choice for this very reason.
+        }
+    }
+};
+int8_t Track::numTracks = 0;
+Track tracks[16] = Track();
 
 class Matrix
 {
 private:
-    static int width, height;
+    static int8_t width, height, lastBlinkTime;
     Adafruit_NeoTrellis trellisArray[matrixHeight / 4][matrixWidth / 4] = {{Adafruit_NeoTrellis(0x2E), Adafruit_NeoTrellis(0x2F)}};
 
 public:
-    int numPixels = width * height;
+    int8_t page, bank, selTrack;
+    int8_t numPixels = width * height;
     static Adafruit_MultiTrellis trellis;
     Matrix()
     {
@@ -53,6 +119,11 @@ public:
         height = matrixHeight;
         Adafruit_NeoTrellis trellisArray[height / 4][width / 4] = {{Adafruit_NeoTrellis(0x2E), Adafruit_NeoTrellis(0x2F)}};
         Adafruit_MultiTrellis trellis((Adafruit_NeoTrellis *)trellisArray, width / 4, height / 4);
+        for (int i = 0; i < numPixels; i++)
+        {
+            trellis.activateKey(i, SEESAW_KEYPAD_EDGE_FALLING, true);
+            trellis.registerCallback(i, keyPressed);
+        }
     }
     void show()
     {
@@ -60,12 +131,90 @@ public:
     }
     void process()
     {
+        drawMatrix();
+    }
+    void bankUp()
+    {
+        if (bank != height - 1)
+        { //-1 because 0 indexing destroys my brain and life
+            selTrack += height;
+        }
+        else
+        {
+            selTrack = 0;
+        }
+    }
+    void bankDown()
+    {
+        if (bank != 0)
+        {
+            selTrack -= height;
+        }
+        else
+        {
+            selTrack = tracks[0].numTracks;
+        }
+    }
+    void drawMatrix()
+    {
+        //figure out bank before page, since loading 16 track objects is significantly slower than loading 4 for the future operations
+        bank = selTrack / height;
+        int8_t bankOffset = bank * height;
+        Track dispTracks[height] = {tracks[bankOffset], tracks[bankOffset + 1], tracks[bankOffset + 2], tracks[bankOffset + 3]}; //TODO: change to using pointers so that future operations are super quick
+
+        //figure out page
+        if (step > width)
+        {
+            page = step / width;
+        }
+
+        for (int8_t y = 0; y < height; y++)
+        {
+            for (int8_t x = page; x < page + 8; x++)
+            {
+                { //selection stuff
+                    if (dispTracks[y].noteSelected(x))
+                    {
+                        if ((millis() / selBlinkTime) % 2 == 0)
+                        {
+                            //turn cell off to blink
+                            trellis.setPixelColor(x, y, 0x000000);
+                            break; //we're done... for now
+                            //if this doesn't work (I don't have all my parts while writing this and it is 4 am so I'm not sure what I'm doing anyways),
+                            //use x++ instead to effetively break this loop but cause some funky things to happen with consecutive selected notes
+                            //or use a flag for when the note is off
+                        }
+                    }
+                } //end selection stuff
+                { //actually draw the cell otherwise
+                    if (dispTracks[y].getNote(x) != 0)
+                    {
+                        int8_t blueVal = 0;
+                        if (selTrack % height == y)
+                        {
+                            blueVal = selTrackBlueOffset; //defined wayyy up there in the constants
+                        }
+                        trellis.setPixelColor(x, y, seesaw_NeoPixel::Color(dispTracks[y].getNote(x) * 2, 255 - dispTracks[y].getNote(x) * 2, 0)); //velocity will bring it from green up to red
+                    }
+                    else if (selTrack % height == y)
+                    {
+                        trellis.setPixelColor(x, y, selTrackOffColor);
+                    }
+                }
+            }
+        }
+    }
+    TrellisCallback keyPressed(keyEvent e)
+    {
+        int x = e.bit.NUM % width;
+        int y = e.bit.NUM / width;
+        //i really hope this works
+        selTrack = (bank * 4) + y;
+        tracks[selTrack].toggleNoteOn((page * width) + x);
     }
 };
-
-//--------------------------------------------------------------------------------------------------------------------------
-//Global Variables
 Matrix matrix = Matrix();
+
 //--------------------------------------------------------------------------------------------------------------------------
 //Setup
 void setup()
